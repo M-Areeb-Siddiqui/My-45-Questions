1df8e13ea7cd03450d5198203f49805bebc80969
M-Areeb-Siddiqui <160623014+M-Areeb-Siddiqui@users.noreply.github.com>
2024-03-07 04:37:48 -0800

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,2 +1,6 @@
-let userName : string= "Eric"
-console.log("Hello ",userName, " would you like to learn some Python today?");
+let personsName : string= "Muhammad Areeb siddiqui"
+//lower case
+console.log(personsName.toLowerCase());
+console.log(personsName.toUpperCase());
+console.log(personsName.charAt(0).toUpperCase()+personsName.slice(1).toLowerCase());
+
